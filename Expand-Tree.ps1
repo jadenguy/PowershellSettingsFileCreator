7b7a75021606c01ce656f57f0b7ea20c6b608902
Alfredo Inostroza <jadenguy@gmail.com>
2018-11-06 12:39:47 -0500

    whitespace

--- Expand-Tree.ps1
+++ Expand-Tree.ps1
@@ -3,15 +3,15 @@ function Expand-Tree ($leaf, $currentRootString, $currentRoot) {
     if ($type -contains "System.Management.Automation.PSCustomObject") {
         $value = "System.Management.Automation.PSCustomObject"
         $contains = $leaf.psobject.properties
+        write-host $contains
         $contains | ForEach-Object {
             $branch = $_
             $branchName = $branch.name
             $branchValue = $branch.value
             if ( $currentRoot) {
-                $nextPathObject= @($currentRoot)
+                $nextPathObject = @($currentRoot)
             }
-            $nextPathObject+= @($branchName)
-            $newCursor = $cursor * $contains.length + $i
+            $nextPathObject += @($branchName)
             $nextPath = "$currentRootString.$branchName"
             Expand-Tree $branchValue $nextPath $nextPathObject
         }
@@ -20,8 +20,8 @@ function Expand-Tree ($leaf, $currentRootString, $currentRoot) {
         $value = $leaf
     }
     $ret += [PSCustomObject]@{
-        path  = $currentRoot
-        value = $value
+        path       = $currentRoot
+        value      = $value
         stringPath = $currentRootString
     }    
     return $ret
