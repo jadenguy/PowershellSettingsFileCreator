b89788d639efc9464b58759377f676d25d7d0e96
Alfredo Inostroza <jadenguy@gmail.com>
2018-11-06 11:47:33 -0500

    Provides a tree that is sortable by the stringPath I still want to collect every return and add it to a final output object

--- Expand-Tree.ps1
+++ Expand-Tree.ps1
@@ -1,26 +1,28 @@
-function Expand-Tree ($leaf, [string]$currentRoot) {
+function Expand-Tree ($leaf, $currentRootString, $currentRoot) {
     $type = $leaf.psobject.typenames
-    if ($type -eq "System.Management.Automation.PSCustomObject") {
-        $value = 'PSCustomObject'
+    if ($type -contains "System.Management.Automation.PSCustomObject") {
+        $value = "System.Management.Automation.PSCustomObject"
         $contains = $leaf.psobject.properties
-        $contains|ForEach-Object {
+        $contains | ForEach-Object {
             $branch = $_
             $branchName = $branch.name
             $branchValue = $branch.value
-            if ($currentRoot) {
-                $branchPath = "$currentRoot.$branchName"
+            if ( $currentRoot) {
+                $nextPathObject= @($currentRoot)
             }
-            else {$branchPath = $branchName}
-            Expand-Tree $branchValue $branchPath
+            $nextPathObject+= @($branchName)
+            $newCursor = $cursor * $contains.length + $i
+            $nextPath = "$currentRootString.$branchName"
+            Expand-Tree $branchValue $nextPath $nextPathObject
         }
-        
     }
     else {
         $value = $leaf
     }
-    $ret = [PSCustomObject]@{
+    $ret += [PSCustomObject]@{
         path  = $currentRoot
         value = $value
+        stringPath = $currentRootString
     }    
     return $ret
 }
