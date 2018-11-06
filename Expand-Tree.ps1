9d961e7913dfd6f969df9a532025dbf1651bfae4
Alfredo Inostroza <jadenguy@gmail.com>
2018-11-06 10:06:54 -0500

    Provides fully broken out list It's in reverse order, want to fix this Maybe ret++ instead of just implicit returns

--- Expand-Tree.ps1
+++ Expand-Tree.ps1
@@ -1,21 +1,26 @@
-function Expand-Tree ($leaf, $currentRoot) {
+function Expand-Tree ($leaf, [string]$currentRoot) {
     $type = $leaf.psobject.typenames
     if ($type -eq "System.Management.Automation.PSCustomObject") {
+        $value = 'PSCustomObject'
         $contains = $leaf.psobject.properties
         $contains|ForEach-Object {
             $branch = $_
             $branchName = $branch.name
             $branchValue = $branch.value
-            $branchPath = "$currentRoot.$branchName"
+            if ($currentRoot) {
+                $branchPath = "$currentRoot.$branchName"
+            }
+            else {$branchPath = $branchName}
             Expand-Tree $branchValue $branchPath
         }
-        $value = 'PSCustomObject'
+        
     }
     else {
         $value = $leaf
     }
-    return     [PSCustomObject]@{
-        path = $currentRoot
+    $ret = [PSCustomObject]@{
+        path  = $currentRoot
         value = $value
-    }
+    }    
+    return $ret
 }
