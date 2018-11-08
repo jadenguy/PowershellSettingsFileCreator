cbeeb339f83e5ab72a4c32d89403542114c93c41
Alfredo Inostroza <jadenguy@gmail.com>
2018-11-08 16:54:43 -0500

    Processes it properly

--- process.ps1
+++ process.ps1
@@ -1,7 +1,7 @@
 . ( join-path $PSScriptRoot "Get-SettingsFile")
 #. ( join-path $PSScriptRoot "Expand-Tree")
 #. ( join-path $PSScriptRoot "Compare-Trees")
-. ( join-path $PSScriptRoot "Compare-Trees2")
+. ( join-path $PSScriptRoot "Update-Tree")
 
 $wantInt = "System.Int32"
 $int = 1
@@ -22,11 +22,20 @@ $missingKeyObject = [PSCustomObject]@{
     StrValue = 'a'
 }
 Write-Host starting
-Compare-Trees2 -wantedObject $wantInt -givenObject $int -passes $true
-Compare-Trees2 -wantedObject $wantInt -givenObject ([ref]$string)
-Compare-Trees2 -wantedObject $wantInt -givenObject ([ref]$missingKeyObject)
-Compare-Trees2 -wantedObject $wantedObject -givenObject ([ref]$validObject) -passes $true
-Compare-Trees2 -wantedObject $wantedObject -givenObject ([ref]$int)
-Compare-Trees2 -wantedObject $wantedObject -givenObject ([ref]$invalidObject)
-Compare-Trees2 -wantedObject $wantedObject -givenObject ([ref]$missingKeyObject)
-write-host done
+function TestABunch {
+    
+    
+    Update-Tree -wantedObject $wantInt -givenObject ([ref]$int) -passes $true
+    Update-Tree -wantedObject $wantInt -givenObject ([ref]$string)
+    Update-Tree -wantedObject $wantInt -givenObject ([ref]$missingKeyObject)
+    Update-Tree -wantedObject $wantedObject -givenObject ([ref]$validObject) -passes $true
+    Update-Tree -wantedObject $wantedObject -givenObject ([ref]$int)
+    Update-Tree -wantedObject $wantedObject -givenObject ([ref]$invalidObject)
+    Update-Tree -wantedObject $wantedObject -givenObject ([ref]$missingKeyObject)
+}
+
+$settingsRequired = Get-SettingsFile "required.json"
+$settingsFileIn = Get-SettingsFile "settings.json"
+Update-Tree $settingsRequired ([ref]$settingsFileIn)
+$settingsFileOut = $settingsFileIn | ConvertTo-Json -Depth 100
+$settingsFileOut
